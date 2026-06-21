cbbf2d66e1242bf7cdebbecc178bde7c0345b849
misamyaghoobian-netizen <misamyaghoobian@gmail.com>
2026-06-21 21:46:58 +0330

    practic 8 pass

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,37 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...theArgs) {
+   for (let j = 0; j < theArgs.length; j++) {
+      
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr.findIndex(n => n===theArgs[j])!==-1) {
+      arr.splice(arr.findIndex(n => n===theArgs[j]),1)      
+        } 
+    }
+}
+ return arr
 };
+
+
+
+
+
+const removeFromArray2 = function(arr,...theArgs) {
+   let Arr = arr.slyce();
+for (let i = 0; i < theArgs.length; i++) {
+Arr = arr.filter(n=>!(arr.includes(theArgs[i])))    
+    
+}
+
+
+
+ return Arr;
+
+ 
+};
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
